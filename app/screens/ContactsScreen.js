de14b7e6bf5a1982597b32ad5b0735ef63ceddbb
DanilsMIT <danilsmitdm@gmail.com>
2024-11-29 08:39:54 -0500

    Ignorar todos los rar, apropiación de ContactsScreen Estudiante 2

--- app/screens/ContactsScreen.js
+++ app/screens/ContactsScreen.js
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet, Button } from 'react-native'
 
 export const Contacts = ({ navigation }) => {
     return <View style={styles.container}>
-        <Text> Soy los cambios del Estudiante DOS 2</Text>
+        <Text> Soy los cambios del Estudiante 2, creditos a su autor original!: E2</Text>
         <Button
             title='HOME'
             onPress={() => {
